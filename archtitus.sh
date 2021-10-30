ce5d5c8b785ca19a062ed9e52cf366be3605c087
Henry <89177279+henry-jacq@users.noreply.github.com>
2021-10-30 23:17:05 +0530

    Updated

--- archtitus.sh
+++ archtitus.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
-    echo "Starting 0-preintall.sh ####"
+    echo "executing "
     bash 0-preinstall.sh
-    echo "[+] 0 is finished ! ####"
-    sleep 4
-    arch-chroot /mnt /root/ArchTitus/1-setup.sh
-    sleep 4
-    echo "==> sourcing /mnt/root/ArchTitus/install.conf ####"
-    source /mnt/root/ArchTitus/install.conf
-    echo "[+] Starting 2-user.sh ! ####"
-    sleep 4
-    arch-chroot /mnt /usr/bin/runuser -u $username -- /home/$username/ArchTitus/2-user.sh
-    echo "[+] Starting 3-post-setup.sh ! ####"
-    sleep 4
-    arch-chroot /mnt /root/ArchTitus/3-post-setup.sh
+    echo "executing 1-setup.sh"
+    arch-chroot /mnt /root/BetterArch/1-setup.sh
+    echo "Sourcing install.conf"
+    source /mnt/root/BetterArch/install.conf
+    echo "executing 2-user.sh"
+    arch-chroot /mnt /usr/bin/runuser -u $username -- /home/$username/BetterArch/2-user.sh
+    echo "executing 3-post-setup.sh"
+    arch-chroot /mnt /root/BetterArch/3-post-setup.sh
