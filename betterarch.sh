bb2ae0632005078769fcc50defc8b12742b45a7c
Henry <89177279+henry-jacq@users.noreply.github.com>
2021-11-03 12:27:15 +0000

    Delete betterarch.sh

--- betterarch.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-    bash 0-preinstall.sh
-    arch-chroot /mnt /root/BetterArch/1-setup.sh
-    source /mnt/root/BetterArch/install.conf
-    arch-chroot /mnt /usr/bin/runuser -u $username -- /home/$username/BetterArch/2-user.sh
-    arch-chroot /mnt /root/BetterArch/3-post-setup.sh
